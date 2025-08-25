e04ad5e1f8657a6b009a09c6e4247a4f755a27aa
Egor Sergeev <egorsergeev@Egors-MacBook-Air-10.local>
2025-08-24 21:59:57 -0400

    fix(ui): unify click/drag/tap so toggling works on both desktop and mobile

--- src/EventPage.jsx
+++ src/EventPage.jsx
@@ -1,1 +1,13 @@
-$(sed 's/onClick={() => handleMouseDown(r, c)}/onClick={() => toggleCell(r, c)}/' src/EventPage.jsx)
+$(awk '
+BEGIN{in_grid=0}
+{
+  if ($0 ~ /function StickyGrid/) in_grid=1
+  if (in_grid) {
+    if ($0 ~ /onMouseDown=\{\(\) => handleMouseDown/) {
+      print $0
+      print "                onClick={() => toggleCell(r, c)}  // mobile tap toggle"
+      next
+    }
+  }
+  print
+}' src/EventPage.jsx)
